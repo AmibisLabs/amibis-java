c6b4c9fba5fe386eb5f14c8c3c4e25199e378dde
reignier <reignier@c4bd1236-c4f7-0310-a2fe-a60683d9aa4a>
2006-03-23 22:05:23 +0000

    First version of TcpClientServer : not tested yet
    git-svn-id: svn+ssh://oberon/svn/jOMiSCID/trunk@215 c4bd1236-c4f7-0310-a2fe-a60683d9aa4a

--- src/fr/prima/omiscid/com/TcpClientServer.java
+++ src/fr/prima/omiscid/com/TcpClientServer.java
@@ -18,7 +18,11 @@
 package fr.prima.omiscid.com;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Vector;
+
+import fr.prima.omiscid.com.interf.OmiscidMessageListener;
 
 /**
  * @author Patrick Reignier (UJF/Gravir)
@@ -27,12 +31,12 @@ import java.util.Vector;
 public class TcpClientServer extends TcpServer {
 	
 	/** list of clients connected to a remote server */
-	protected Vector<TcpClient> listOfClients = null ;
+	protected HashMap<Integer, TcpClient> listOfClients = null ;
 	
 	public TcpClientServer(int serviceId) throws IOException
 	{
 		super(serviceId, 0) ;
-		listOfClients = new Vector<TcpClient>() ;
+		listOfClients = new HashMap<Integer, TcpClient>() ;
 	}
 	
 	/**
@@ -52,9 +56,153 @@ public class TcpClientServer extends TcpServer {
 		}
 		synchronized(this)
 		{
-			listOfClients.add(tcpClient) ;
+			listOfClients.put(tcpClient.getPeerId(), tcpClient) ;
 		}
 		
 		return tcpClient.getPeerId() ;
+	}
+	
+	/* (non-Javadoc)
+	 * @see fr.prima.omiscid.com.TcpServer#sendToClients(byte[])
+	 */
+	@Override
+	public void sendToClients(byte[] buffer) {
+		// TODO Auto-generated method stub
+		super.sendToClients(buffer);
+		
+		synchronized (this)
+		{
+			for (TcpClient client : listOfClients.values())
+			{
+				client.send(buffer) ;
+			}
+		}
+	}
+	
+	/**
+	 * Sends a buffer to a particular client
+	 * @param buffer the buffer to send
+	 * @param pid the peer id
+	 * @return true if the client exists 
+	 */
+	public boolean sendToOneClient(byte[] buffer,int pid)
+	{
+		TcpClient client = listOfClients.get(pid) ;
+		if (client != null)
+		{
+			client.send(buffer) ;
+			return true ;
+		}
+		else
+			return super.sendToOneClient(buffer, pid) ;
+	}
+	
+	/* (non-Javadoc)
+	 * @see fr.prima.omiscid.com.TcpServer#addOmiscidMessageListener(fr.prima.omiscid.com.interf.OmiscidMessageListener)
+	 */
+	@Override
+	public void addOmiscidMessageListener(OmiscidMessageListener listener) {
+		// TODO Auto-generated method stub
+		super.addOmiscidMessageListener(listener);
+		
+		synchronized(this)
+		{
+			cleanListOfClients() ;
+			for (TcpClient client : listOfClients.values())
+			{
+				if (client.isConnected())
+					client.addOmiscidMessageListener(listener) ;
+			}
+		}
+	}
+	
+	/* (non-Javadoc)
+	 * @see fr.prima.omiscid.com.TcpServer#getNbConnections()
+	 */
+	@Override
+	public int getNbConnections() {
+		// TODO Auto-generated method stub
+		cleanListOfClients() ;
+		return super.getNbConnections() + listOfClients.size() ;
+	}
+	
+	
+	
+	/**
+	 * Removes unconnected clients
+	 */
+	protected synchronized void cleanListOfClients()
+	{
+		Vector<TcpClient> remove = new Vector<TcpClient>() ;
+		
+		for (TcpClient client : listOfClients.values())
+		{
+			if (!client.isConnected())
+				remove.add(client) ;
+		}
+		
+		for (TcpClient clientToRemove : remove)
+			listOfClients.remove(clientToRemove) ;
+	}
+	
+	/* (non-Javadoc)
+	 * @see fr.prima.omiscid.com.TcpServer#getPeerId(java.util.Vector)
+	 */
+	@Override
+	public int getPeerId(Vector<Integer> vec) {
+		// TODO Auto-generated method stub
+		int nb = super.getPeerId(vec);
+		
+		synchronized(this)
+		{
+			for (TcpClient client : listOfClients.values())
+			{
+				nb++ ;
+				vec.add(client.getPeerId()) ;
+			}
+		}
+		
+		return nb ;
+	}
+	
+	/* (non-Javadoc)
+	 * @see fr.prima.omiscid.com.TcpServer#removeOmiscidMessageListener(fr.prima.omiscid.com.interf.OmiscidMessageListener)
+	 */
+	@Override
+	public void removeOmiscidMessageListener(OmiscidMessageListener listener) {
+		// TODO Auto-generated method stub
+		cleanListOfClients() ;
+		super.removeOmiscidMessageListener(listener);
+		
+		synchronized(this)
+		{
+			for (TcpClient client : listOfClients.values())
+				client.removeOmiscidMessageListener(listener) ;
+		}
+	}
+	
+	/* (non-Javadoc)
+	 * @see fr.prima.omiscid.com.TcpServer#isStillConnected(int)
+	 */
+	@Override
+	public boolean isStillConnected(int peerId) {
+		// TODO Auto-generated method stub
+		boolean isConnected = super.isStillConnected(peerId);
+		if (isConnected)
+			return true ;
+		else
+		{
+			synchronized(this)
+			{
+				cleanListOfClients() ;
+				for (TcpClient client : listOfClients.values())
+				{
+					isConnected = client.isConnected() ;
+					if (isConnected)
+						return true ;
+				}
+			}
+			return false ;
+		}
 	}
 }
