c42d16bc2c0ca13f145c72a9a492c3da9f240a6d
emonet <emonet@c4bd1236-c4f7-0310-a2fe-a60683d9aa4a>
2006-07-06 08:16:44 +0000

    nothing
    git-svn-id: svn+ssh://oberon/svn/jOMiSCID/trunk@878 c4bd1236-c4f7-0310-a2fe-a60683d9aa4a

--- src/fr/prima/omiscid/control/interf/GlobalConstants.java
+++ src/fr/prima/omiscid/control/interf/GlobalConstants.java
@@ -1,18 +1,18 @@
 package fr.prima.omiscid.control.interf;
 
 public final class GlobalConstants {
-    
+
     public static final String connectorTypeInputXMLTag = "input";
     public static final String connectorTypeOutputXMLTag = "output";
     public static final String connectorTypeInOutputXMLTag = "inoutput";
-    
+
     public static final String variableAccessTypeConstant = "constant";
     public static final String variableAccessTypeRead= "read";
     public static final String variableAccessTypeReadWrite = "readWrite";
 
     public static final String dnssdDefaultWorkingDomain = "_bip._tcp";
     public static final String dnssdWorkingDomainEnvironmentVariableName = "OMISCID_WORKING_DOMAIN";
-    
+
     public static final String constantNameForPeerId = "id";
     public static final String constantNameForOwner = "owner";
     public static final String constantNameForClass = "class";
@@ -21,7 +21,7 @@ public final class GlobalConstants {
     public static final String keyForFullTextRecord = "desc";
     public static final String keyForFullTextRecordFull = "full";
     public static final String keyForFullTextRecordNonFull = "part";
-    
+
     public static final String prefixForInputInDnssd = "i/";
     public static final String prefixForOutputInDnssd = "o/";
     public static final String prefixForInoutputInDnssd = "d/";
