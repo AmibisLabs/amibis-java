4f7f5c9adcee0a047655066003b1d4fbf6cdb778
emonet <emonet@c4bd1236-c4f7-0310-a2fe-a60683d9aa4a>
2006-09-11 14:44:00 +0000

    continuation of previous commit added new classes generated by castor
    git-svn-id: svn+ssh://oberon/svn/jOMiSCID/trunk@1267 c4bd1236-c4f7-0310-a2fe-a60683d9aa4a

---
+++ generated-src/fr/prima/omiscid/control/message/query/FullDescription.java
@@ -0,0 +1,117 @@
+/*
+ * This class was automatically generated with 
+ * <a href="http://www.castor.org">Castor 1.0.1</a>, using an XML
+ * Schema.
+ * $Id$
+ */
+
+package fr.prima.omiscid.control.message.query;
+
+  //---------------------------------/
+ //- Imported classes and packages -/
+//---------------------------------/
+
+import java.io.IOException;
+import java.io.Reader;
+import java.io.Serializable;
+import java.io.Writer;
+import org.exolab.castor.xml.MarshalException;
+import org.exolab.castor.xml.Marshaller;
+import org.exolab.castor.xml.Unmarshaller;
+import org.exolab.castor.xml.ValidationException;
+import org.xml.sax.ContentHandler;
+
+/**
+ * Class FullDescription.
+ * 
+ * @version $Revision$ $Date$
+ */
+public class FullDescription implements java.io.Serializable {
+
+
+      //----------------/
+     //- Constructors -/
+    //----------------/
+
+    public FullDescription() 
+     {
+        super();
+    } //-- fr.prima.omiscid.control.message.query.FullDescription()
+
+
+      //-----------/
+     //- Methods -/
+    //-----------/
+
+    /**
+     * Method isValid
+     * 
+     * 
+     * 
+     * @return boolean
+     */
+    public boolean isValid()
+    {
+        try {
+            validate();
+        }
+        catch (org.exolab.castor.xml.ValidationException vex) {
+            return false;
+        }
+        return true;
+    } //-- boolean isValid() 
+
+    /**
+     * Method marshal
+     * 
+     * 
+     * 
+     * @param out
+     */
+    public void marshal(java.io.Writer out)
+        throws org.exolab.castor.xml.MarshalException, org.exolab.castor.xml.ValidationException
+    {
+        
+        Marshaller.marshal(this, out);
+    } //-- void marshal(java.io.Writer) 
+
+    /**
+     * Method marshal
+     * 
+     * 
+     * 
+     * @param handler
+     */
+    public void marshal(org.xml.sax.ContentHandler handler)
+        throws java.io.IOException, org.exolab.castor.xml.MarshalException, org.exolab.castor.xml.ValidationException
+    {
+        
+        Marshaller.marshal(this, handler);
+    } //-- void marshal(org.xml.sax.ContentHandler) 
+
+    /**
+     * Method unmarshal
+     * 
+     * 
+     * 
+     * @param reader
+     * @return FullDescription
+     */
+    public static fr.prima.omiscid.control.message.query.FullDescription unmarshal(java.io.Reader reader)
+        throws org.exolab.castor.xml.MarshalException, org.exolab.castor.xml.ValidationException
+    {
+        return (fr.prima.omiscid.control.message.query.FullDescription) Unmarshaller.unmarshal(fr.prima.omiscid.control.message.query.FullDescription.class, reader);
+    } //-- fr.prima.omiscid.control.message.query.FullDescription unmarshal(java.io.Reader) 
+
+    /**
+     * Method validate
+     * 
+     */
+    public void validate()
+        throws org.exolab.castor.xml.ValidationException
+    {
+        org.exolab.castor.xml.Validator validator = new org.exolab.castor.xml.Validator();
+        validator.validate(this);
+    } //-- void validate() 
+
+}
