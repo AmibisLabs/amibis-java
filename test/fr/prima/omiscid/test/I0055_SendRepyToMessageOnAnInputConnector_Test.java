f147dc05aad80514e69a675d2c96f11a2a822312
emonet <emonet@c4bd1236-c4f7-0310-a2fe-a60683d9aa4a>
2008-04-01 11:39:10 +0000

    added failing tests after Anne Gerodolle s questions on sending on input connector
    git-svn-id: svn+ssh://oberon/svn/jOMiSCID/trunk@4356 c4bd1236-c4f7-0310-a2fe-a60683d9aa4a

---
+++ test/fr/prima/omiscid/test/I0055_SendRepyToMessageOnAnInputConnector_Test.java
@@ -0,0 +1,94 @@
+/**
+ * Copyright© 2005-2006 INRIA/Université Pierre Mendès-France/Université Joseph Fourier.
+ *
+ * O3MiSCID (aka OMiSCID) Software written by Sebastien Pesnel, Dominique
+ * Vaufreydaz, Patrick Reignier, Remi Emonet and Julien Letessier.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package fr.prima.omiscid.test;
+
+
+import fr.prima.omiscid.user.connector.ConnectorListener;
+import fr.prima.omiscid.user.connector.ConnectorType;
+import fr.prima.omiscid.user.connector.Message;
+import fr.prima.omiscid.user.exception.IncorrectConnectorType;
+import java.io.IOException;
+import fr.prima.omiscid.user.service.Service;
+import fr.prima.omiscid.user.service.ServiceFactory;
+import fr.prima.omiscid.user.service.ServiceFilters;
+import fr.prima.omiscid.user.service.ServiceProxy;
+import fr.prima.omiscid.user.util.Utility;
+
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class I0055_SendRepyToMessageOnAnInputConnector_Test {
+    
+    @Test(expected=TestPassedPseudoException.class)
+    public void doIt() throws IOException, InterruptedException {
+        ServiceFactory factory = FactoryFactory.factory();
+        {
+            final Service server = factory.create("I0055Server");
+            server.addConnector("bug", "", ConnectorType.OUTPUT);
+            server.addConnectorListener("bug", new ConnectorListener() {
+
+                public void messageReceived(Service service, String localConnectorName, Message message) {
+                    try {
+                        service.sendReplyToMessage(Utility.message("hohoho!!!"), message);
+                    } catch (IncorrectConnectorType e) {
+                        FactoryFactory.passed("Exception properly thrown");
+                    }
+                }
+
+                public void disconnected(Service service, String localConnectorName, int peerId) {
+                }
+
+                public void connected(Service service, String localConnectorName, int peerId) {
+                }
+            });
+            server.start();
+        }{
+            Service client = factory.create("I0055lient");
+            client.addConnector("bug", "", ConnectorType.INPUT);
+            client.start();
+            client.addConnectorListener("bug", new ConnectorListener() {
+                public void messageReceived(Service service, String localConnectorName, Message message) {
+                    FactoryFactory.failed("Message received but should not: "+message.getBufferAsStringUnchecked());
+                }
+                public void disconnected(Service service, String localConnectorName, int peerId) {
+                }
+                public void connected(Service service, String localConnectorName, int peerId) {
+                }
+            });
+            final ServiceProxy proxy = client.findService(ServiceFilters.nameIs("I0055Server"));
+            client.connectTo("bug", proxy, "bug");
+            client.sendToAllClients("bug", Utility.stringToByteArray("hiiiii"));
+            try {
+                Thread.sleep(150);
+            } catch (InterruptedException e) {}
+            client.stop();
+        }
+        FactoryFactory.waitResult(1500);
+    }
+
+}
