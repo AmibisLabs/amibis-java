1a96f110c84c466faffa650f6b6ac79b9ca30f91
emonet <emonet@c4bd1236-c4f7-0310-a2fe-a60683d9aa4a>
2008-07-08 15:11:19 +0000

    reorganized release script before changing it to maven (bundlewise)
    git-svn-id: svn+ssh://oberon/svn/jOMiSCID/trunk@4698 c4bd1236-c4f7-0310-a2fe-a60683d9aa4a

--- tools.mk
+++ tools.mk
@@ -3,62 +3,76 @@ include release.conf
 
 BASEJAR=omiscid.jar
 INTERFACEJAR=omiscidService.jar
+m=make -f tools.mk
 
-dist: prepare
-# builds the omiscid jar (BASEJAR) file using 'prepare'
+remote-dist: 
+	$m clean-package-build
+	$m clean-bundle-build
 
-# cleans the concerned destinations
-	ssh $(release-server) rm -rf $(release-server-path)/$(release-version)
-	ssh $(release-server) mkdir -p $(release-server-path)/$(release-version)
-	rm -rf ,,rel
-	mkdir  ,,rel
-	rm -rf ,,rel-bundle
-	mkdir  ,,rel-bundle
+	$m omiscid-jar
+	$m bundle-zip
+	$m gui-jar
+	$m release-package
 
-# builds the gui and replicates it
-	make -f tools.mk gui
-	scp ,,rel/$(release-omiscid-gui-jar) $(release-diston)/$(release-version)/
-# copies other distribution files both locally and remotely
-	scp $(BASEJAR) ,,rel/$(release-omiscid-jar)
-	scp $(BASEJAR) $(release-diston)/$(release-version)/$(release-omiscid-jar)
-	scp licences $(patsubst %,lib/%,$(release-jars)) $(patsubst %,lib/%,$(release-libs)) ,,rel/
-	scp licences $(patsubst %,lib/%,$(release-jars)) $(patsubst %,lib/%,$(release-libs)) $(release-diston)/$(release-version)/
-# builds the zip version and replicates it remotely
-	mv ,,rel/ omiscid && zip -r $(release-all-zip) omiscid/ && mv omiscid/ ,,rel
-#	cd ,,rel/ && tar cvfz ../$(release-all-zip) *
-	scp $(release-all-zip) $(release-diston)/
+	$m clean-destination
 
-# builds the bundle zip
-	make -f tools.mk bundle
-	cd ,,rel-bundle && zip -r ../$(release-bundle-zip) *
-	scp $(release-bundle-zip) $(release-diston)/
-# copy the interface jar
-	scp $(INTERFACEJAR) $(release-diston)/
+	$m omiscid-upload
+	$m bundle-upload
+	$m release-package-upload
+	$m gui-upload
 
-# displays a warning if domain is wrong
-	make -f tools.mk checkdomain
+# displays a warning if default domain is wrong
+	$m checkdomain
 
-gui:
-	make -C ../jOMiSCIDGui -f dist.mk BASEJAR=../jOMiSCID/,,rel/$(release-omiscid-gui-jar) prepare
+headers:
+	find src -name \*.java -exec ./set-header.sh {} \;
+	find dnssd-src -name \*.java -exec ./set-header.sh {} \;
+	find user-level-src -name \*.java -exec ./set-header.sh {} \;
+	find test -name \*.java -exec ./set-header.sh {} \;
+	find other-test -name \*.java -exec ./set-header.sh {} \;
 
-bundle:
-# regenerate the libs used by the bundle
-	rm -rf ../jOMiSCIDBundle/embeddedlibs/*
-	rm -rf ../jOMiSCIDBundle/native/*
-	scp ,,rel/$(release-omiscid-jar) $(patsubst %,lib/%,$(release-bundle-jars)) ../jOMiSCIDBundle/embeddedlibs/
-	scp $(patsubst %,lib/%,$(release-libs)) ../jOMiSCIDBundle/native/
-# clean the build dir
-	rm -rf ../jOMiSCIDBundle/build
-# patch manifest file
-	cat ../jOMiSCIDBundle/manifest/manifest.mf | awk "/^Bundle-Version:/ {print \"Bundle-Version: $(release-bundle-version)\" ; next} /^Bundle-ClassPath:/ {print \"Bundle-ClassPath: $(release-bundle-classpath)\" ; next} /^Bundle-NativeCode:/ {print \"Bundle-NativeCode: $(release-bundle-native-code)\" ; next} // {print}" > ,,.mf
-	cp ,,.mf ../jOMiSCIDBundle/manifest/manifest.mf
-# call ant
-	cd ../jOMiSCIDBundle/ && ant -f build.publish.xml -Dosgi.publish.dir=../jOMiSCID/,,rel-bundle/
+
+
+
 
 checkdomain:
 	@find src -name \*.java -exec egrep -e '_bip.+\._tcp' {} \; -exec echo "!!!!!!!!!!!!!!!!!!!!" \; -exec echo "!! DOMAIN WARNING !!" \; -exec echo "!!!!!!!!!!!!!!!!!!!!" \;
 
-prepare:
+omiscid-upload:
+	scp $(BASEJAR) $(release-diston)/$(release-version)/$(release-omiscid-jar)
+	scp licences $(patsubst %,lib/%,$(release-jars)) $(patsubst %,lib/%,$(release-libs)) $(release-diston)/$(release-version)/
+
+bundle-upload:
+	scp $(release-bundle-zip) $(release-diston)/
+# copy the interface jar
+	scp $(INTERFACEJAR) $(release-diston)/
+
+release-package-upload:
+	scp $(release-all-zip) $(release-diston)/
+
+gui-upload:
+	scp ,,rel/$(release-omiscid-gui-jar) $(release-diston)/$(release-version)/
+
+
+clean-bundle-build:
+	rm -rf ,,rel-bundle
+	mkdir  ,,rel-bundle
+
+clean-package-build:
+	rm -rf ,,rel
+	mkdir  ,,rel
+
+clean-destination:
+# cleans the concerned destinations
+	ssh $(release-server) rm -rf $(release-server-path)/$(release-version)
+	ssh $(release-server) mkdir -p $(release-server-path)/$(release-version)
+
+release-package:
+	cp $(BASEJAR) ,,rel/$(release-omiscid-jar)
+	cp licences $(patsubst %,lib/%,$(release-jars)) $(patsubst %,lib/%,$(release-libs)) ,,rel/
+	mv ,,rel/ omiscid && zip -r $(release-all-zip) omiscid/ && mv omiscid/ ,,rel
+
+omiscid-jar:
 	rm -rf ,,build
 	ant -Dfile.encoding=utf-8 compile
 	cp -r build/classes ,,build
@@ -77,12 +91,25 @@ prepare:
 		)
 	cd ,,build && jar cvf ../$(INTERFACEJAR) .
 
-headers:
-	find src -name \*.java -exec ./set-header.sh {} \;
-	find dnssd-src -name \*.java -exec ./set-header.sh {} \;
-	find user-level-src -name \*.java -exec ./set-header.sh {} \;
-	find test -name \*.java -exec ./set-header.sh {} \;
-	find other-test -name \*.java -exec ./set-header.sh {} \;
+gui-jar:
+	make -C ../jOMiSCIDGui -f dist.mk BASEJAR=../jOMiSCID/,,rel/$(release-omiscid-gui-jar) prepare
+
+bundle-zip:
+# regenerate the libs used by the bundle
+	rm -rf ../jOMiSCIDBundle/embeddedlibs/*
+	rm -rf ../jOMiSCIDBundle/native/*
+	cp $(BASEJAR) ../jOMiSCIDBundle/embeddedlibs/$(release-omiscid-gui-jar)
+	cp $(patsubst %,lib/%,$(release-bundle-jars)) ../jOMiSCIDBundle/embeddedlibs/
+	cp $(patsubst %,lib/%,$(release-libs)) ../jOMiSCIDBundle/native/
+# clean the build dir
+	rm -rf ../jOMiSCIDBundle/build
+# patch manifest file
+	cat ../jOMiSCIDBundle/manifest/manifest.mf | awk "/^Bundle-Version:/ {print \"Bundle-Version: $(release-bundle-version)\" ; next} /^Bundle-ClassPath:/ {print \"Bundle-ClassPath: $(release-bundle-classpath)\" ; next} /^Bundle-NativeCode:/ {print \"Bundle-NativeCode: $(release-bundle-native-code)\" ; next} // {print}" > ,,.mf
+	cp ,,.mf ../jOMiSCIDBundle/manifest/manifest.mf
+# call ant
+	cd ../jOMiSCIDBundle/ && ant -f build.publish.xml -Dosgi.publish.dir=../jOMiSCID/,,rel-bundle/
+	cd ,,rel-bundle && zip -r ../$(release-bundle-zip) *
+
 
 #just a bunch of commands used for tests refactoring (conversion to junit)
 # do-replacement 's@public class I0@\nimport org.junit.Test;\nimport static org.junit.Assert.*;\n\npublic class I0@g' I00{0,1,2,3,4}*java
