0f874c1c0bf820c43415a52eb95e568a969bccec
emonet <emonet@c4bd1236-c4f7-0310-a2fe-a60683d9aa4a>
2008-07-09 14:02:11 +0000

    updated release process to use maven and generated felix compatible omiscid bundle
    git-svn-id: svn+ssh://oberon/svn/jOMiSCID/trunk@4699 c4bd1236-c4f7-0310-a2fe-a60683d9aa4a

--- tools.mk
+++ tools.mk
@@ -6,23 +6,27 @@ INTERFACEJAR=omiscidService.jar
 m=make -f tools.mk
 
 remote-dist: 
+	$m ssh-test
 	$m clean-package-build
-	$m clean-bundle-build
+	$m clean-maven-build
+	#$m clean-bundle-build
 
 	$m omiscid-jar
-	$m bundle-zip
+	#$m bundle-zip
 	$m gui-jar
 	$m release-package
+	$m maven-build
 
 	$m clean-destination
 
 	$m omiscid-upload
-	$m bundle-upload
+	#$m bundle-upload
 	$m release-package-upload
 	$m gui-upload
 
 # displays a warning if default domain is wrong
 	$m checkdomain
+
 
 headers:
 	find src -name \*.java -exec ./set-header.sh {} \;
@@ -33,7 +37,8 @@ headers:
 
 
 
-
+ssh-test:
+	@ssh $(release-server) echo SSH OK
 
 checkdomain:
 	@find src -name \*.java -exec egrep -e '_bip.+\._tcp' {} \; -exec echo "!!!!!!!!!!!!!!!!!!!!" \; -exec echo "!! DOMAIN WARNING !!" \; -exec echo "!!!!!!!!!!!!!!!!!!!!" \;
@@ -54,6 +59,9 @@ gui-upload:
 	scp ,,rel/$(release-omiscid-gui-jar) $(release-diston)/$(release-version)/
 
 
+clean-maven-build:
+	rm -rf ,,rel-maven
+
 clean-bundle-build:
 	rm -rf ,,rel-bundle
 	mkdir  ,,rel-bundle
@@ -66,6 +74,18 @@ clean-destination:
 # cleans the concerned destinations
 	ssh $(release-server) rm -rf $(release-server-path)/$(release-version)
 	ssh $(release-server) mkdir -p $(release-server-path)/$(release-version)
+
+maven-build:
+	mkdir  ,,rel-maven
+	cat pom.core.xml | sed 's/@@@v@@@/'$(release-version)'/g' > ,,rel-maven/pom.xml
+	cp $(BASEJAR) ,,rel-maven/$(release-omiscid-jar)
+	cd ,,rel-maven && mvn deploy:deploy-file -Dfile=$(release-omiscid-jar) -DpomFile=pom.xml -DrepositoryId=archiva.internal -Durl=dav:http://oberon:8080/archiva/repository/internal/ 
+
+	cat pom.bundle.xml | sed 's/@@@v@@@/'$(release-version)'/g' | sed 's/@@@bv@@@/'$(release-bundle-version)'/g' > ,,rel-maven/pom.xml
+	cp metadata.xml ,,rel-maven
+	cd ,,rel-maven && mvn package -DpomFile=pom.xml
+	cd ,,rel-maven && mvn deploy:deploy-file -Dfile=target/jomiscid-bundle-$(release-bundle-version).jar -DpomFile=pom.xml -DrepositoryId=prima.obr -Durl=scpexe://oberon/local_home/local/obr/
+	cd ,,rel-maven && mvn org.apache.felix:maven-bundle-plugin:1.4.1:deploy-file -DremoteOBR -Dfile=target/jomiscid-bundle-$(release-bundle-version).jar -DpomFile=pom.xml -DrepositoryId=prima.obr -Duserauth.publickey= -Durl=scpexe://oberon/local_home/local/obr/
 
 release-package:
 	cp $(BASEJAR) ,,rel/$(release-omiscid-jar)
@@ -126,3 +146,31 @@ tests:
 #	ant test | egrep -e '(\[junit\] Testsuite: )|(Failures: )' | sed 's/Tests run: 1\, Failures: 0\, Errors: 0,/================================= OK with: /g'
 #	ant -Dfile.encoding=utf-8 jar
 #	@./tests.sh "$(patsubst %,lib/%,$(release-jars))" ${from}
+
+
+
+#.m2/setting.xml
+# <settings>
+#     <mirrors>
+# 	<mirror>
+# 	    <id>archiva.default</id>
+# 	    <url>http://oberon:8080/archiva/repository/internal/</url>
+# 	    <mirrorOf>*</mirrorOf>
+# 	</mirror>
+#     </mirrors>
+#     <servers>
+# 	<server>
+# 	    <id>archiva.internal</id>
+# 	    <username>prima</username>
+# 	    <password>prima1</password>
+# 	</server>
+# 	<server>
+# 	    <id>archiva.snapshots</id>
+# 	    <username>prima</username>
+# 	    <password>prima1</password>
+#       	</server>	
+# 	<server>
+# 	    <id>prima.obr</id>
+# 	</server>
+#     </servers>
+# </settings>
