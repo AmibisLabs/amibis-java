a858f5b71dd1516ccee94fde07133c245eb9a67f
emonet <emonet@c4bd1236-c4f7-0310-a2fe-a60683d9aa4a>
2009-06-18 15:42:34 +0000

    mavenomiscid: raw merge
    git-svn-id: svn+ssh://oberon/svn/jOMiSCID/branches/jOMiSCID-Maven@7084 c4bd1236-c4f7-0310-a2fe-a60683d9aa4a

---
+++ src/main/java/fr/prima/omiscid/test/App.java
@@ -0,0 +1,13 @@
+package fr.prima.omiscid.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
